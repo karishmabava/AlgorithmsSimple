64f355a25c03d73be6457fb8893dff71485209d0
PUNEET KUMAR <kbava@mail.sfsu.edu>
2017-04-17 13:37:40 -0400

    BirthDayCakeCandles

---
+++ BirthdayCakeCandles.c
@@ -0,0 +1,65 @@
+/* Question:
+ * Colleen is turning  years old! She has  candles of various heights on her cake, and candle  has height . Because the taller candles tower over the shorter ones, Colleen can only blow out the tallest candles.
+
+Given the  for each individual candle, find and print the number of candles she can successfully blow out.
+
+Input Format
+
+The first line contains a single integer, , denoting the number of candles on the cake. 
+The second line contains  space-separated integers, where each integer  describes the height of candle .
+
+Constraints
+
+Output Format
+
+Print the number of candles Colleen blows out on a new line.
+
+Sample Input 0
+
+4
+3 2 1 3
+Sample Output 0
+
+2
+Explanation 0
+
+We have one candle of height , one candle of height , and two candles of height . Colleen only blows out the tallest candles, meaning the candles where . Because there are  such candles, we print  on a new line. */
+
+// Complexity O(n)
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+int main(){
+    int n; 
+    scanf("%d",&n);
+    int *height = malloc(sizeof(int) * n);
+    for(int height_i = 0; height_i < n; height_i++){
+       scanf("%d",&height[height_i]);
+    }
+    
+    // This question simply requires to find the maximum first and then 
+    // The number of occurence of that maximum number
+    
+    int max=0;
+    for(int i=0; i<n; i++){
+        if(max < height[i]){
+           max = height[i]; 
+        }
+    }
+    
+   // Now we need to find the occurence of that number
+    int count=0;
+    for(int i=0; i<n; i++){
+        if(max == height[i])  
+           count++; 
+    }    
+    
+    printf("%d", count);
+    return 0;
+}
+
